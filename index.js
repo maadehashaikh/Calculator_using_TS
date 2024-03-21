ea7ac5291e7c40b22d93be4f29373fb0e752c2cb
Maadeha shaikh <maadeham@gmail.com>
2024-03-22 03:26:11 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+#! /usr/bin/env node
+// called as shabang
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    { message: "Enter first number", type: "number", name: "FisrtNumber" },
+    { message: "Enter second number", type: "number", name: "SecondNumber" },
+    { message: "Select one of these operators to perform action",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Subtraction", "Multiplication", "Division"] },
+]);
+console.log(answer);
+// conditional statement 
+if (answer.operator === "Addition") {
+    console.log(`Your value is ${answer.FisrtNumber + answer.SecondNumber}`);
+}
+else if (answer.operator === "Subtraction") {
+    console.log(`Your value is ${answer.FisrtNumber - answer.SecondNumber}`);
+}
+else if (answer.operator === "Multiplication") {
+    console.log(`Your value is ${answer.FisrtNumber * answer.SecondNumber}`);
+}
+else if (answer.operator === "Division") {
+    console.log(`Your value is ${answer.FisrtNumber / answer.SecondNumber}`);
+}
+else {
+    console.log("Please select Valid Operator");
+}
